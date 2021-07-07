3a9134c95f6d2287b0ed2fa5dc3c32fff4a0c0ef
Damien DURO <damien@weweb.io>
2021-07-07 11:13:16 +0200

    Version 3.0.0

--- src/components/CollectionEdit.vue
+++ src/components/CollectionEdit.vue
@@ -5,40 +5,37 @@
                 type="text"
                 name="url"
                 placeholder="https://feed-url.com/endpoint"
-                :value="feed.url"
-                @input="setProp('url', $event)"
-                v-on:keyup.native.enter="$emit('next')"
+                :model-value="feed.url"
                 large
+                @update:modelValue="setProp('url', $event)"
             />
         </wwEditorFormRow>
         <wwEditorFormRow label="Headers">
-            <template slot="append-label">
-                <button class="ww-editor-button -primary -small m-auto-left m-bottom" @click="addHeader">
+            <template #append-label>
+                <button type="button" class="ww-editor-button -primary -small m-auto-left m-bottom" @click="addHeader">
                     Add header field
                 </button>
             </template>
             <div
-                class="rss-feed-collection-edit__row -space-between m-bottom"
                 v-for="(header, index) in feed.headers"
                 :key="index"
+                class="rss-feed-collection-edit__row -space-between m-bottom"
             >
                 <wwEditorFormInput
                     type="text"
                     name="key"
                     placeholder="Key"
-                    :value="header.key"
-                    @input="setHeaderProp('key', $event)"
-                    v-on:keyup.native.enter="$emit('next')"
+                    :model-value="header.key"
+                    @update:modelValue="setHeaderProp('key', $event)"
                 />
                 <wwEditorFormInput
                     type="text"
                     name="value"
                     placeholder="Value"
-                    :value="header.value"
-                    @input="setHeaderProp('value', $event)"
-                    v-on:keyup.native.enter="$emit('next')"
+                    :model-value="header.value"
+                    @update:modelValue="setHeaderProp('value', $event)"
                 />
-                <button class="ww-editor-button -tertiary -small -icon -red" @click="deleteHeader(index)">
+                <button type="button" class="ww-editor-button -tertiary -small -icon -red" @click="deleteHeader(index)">
                     <wwEditorIcon class="ww-editor-button-icon" name="delete" small />
                 </button>
             </div>
@@ -49,21 +46,10 @@
 <script>
 export default {
     props: {
-        plugin: { type: Object, required: true },
         config: { type: Object, required: true },
     },
-    watch: {
-        isSetup: {
-            immediate: true,
-            handler(value) {
-                this.$emit('update-is-valid', value);
-            },
-        },
-    },
+    emits: ['update:config'],
     computed: {
-        isSetup() {
-            return !!this.feed.url;
-        },
         feed() {
             return {
                 url: undefined,
@@ -89,7 +75,7 @@ export default {
             this.setProp('headers', headers);
         },
         setProp(key, value) {
-            this.$emit('update-config', { ...this.feed, [key]: value });
+            this.$emit('update:config', { ...this.feed, [key]: value });
         },
     },
 };
